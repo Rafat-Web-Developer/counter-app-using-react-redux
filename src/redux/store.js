969c7f179a561ccc08e4ad96282d8461c795d75d
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-08-27 17:16:35 +0600

    Get upcomming state

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,6 +5,9 @@ const myOwnMiddleware = (store) => (next) => (action) => {
   console.log(`Action : ${JSON.stringify(action)}`);
   console.log(`Before : ${JSON.stringify(store.getState())}`);
 
+  const upcomingState = [action].reduce(rootReducer, store.getState());
+  console.log(`Upcoming State: ${JSON.stringify(upcomingState)}`);
+
   return next(action);
 };
 
