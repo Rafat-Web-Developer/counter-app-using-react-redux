e77be754255f48080ac16702dbc3b0d593957617
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-08-27 09:42:58 +0600

    Complete counter app in older way

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,14 +1,19 @@
-import React from "react";
+import { connect } from "react-redux";
+import { decrement, increment } from "../redux/counter/actions";
 
-const Counter = () => {
+const Counter = ({ count, increment, decrement }) => {
   return (
     <div className='p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow'>
-      <div className='text-2xl font-semibold'>0</div>
+      <div className='text-2xl font-semibold'>{count}</div>
       <div className='flex space-x-3'>
-        <button className='bg-indigo-400 text-white px-3 py-2 rounded shadow'>
+        <button
+          className='bg-indigo-400 text-white px-3 py-2 rounded shadow'
+          onClick={() => increment(5)}>
           Increment
         </button>
-        <button className='bg-red-400 text-white px-3 py-2 rounded shadow'>
+        <button
+          className='bg-red-400 text-white px-3 py-2 rounded shadow'
+          onClick={() => decrement(5)}>
           Decrement
         </button>
       </div>
@@ -16,4 +21,17 @@ const Counter = () => {
   );
 };
 
-export default Counter;
+const mapStateToProps = (state) => {
+  return {
+    count: state.value,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    increment: (value) => dispatch(increment(value)),
+    decrement: (value) => dispatch(decrement(value)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
